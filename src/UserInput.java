19d84f76a0570987f71f1da7fd5ea5232ac53002
darsh <darsh@DESKTOP-MG00CVE>
2023-07-20 23:45:24 +0530

    Fourth Commit

---
+++ src/UserInput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int a=sc.nextInt();
+		System.out.println("Number is "+a);
+
+	}
+
+}
